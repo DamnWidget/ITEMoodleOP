3aa2ff7db990176f41e2a98e184d858184ec71f6
Oscar Campos <oscar.campos@open-phoenix.com>
2011-10-10 09:56:51 +0200

    Adición de Euskera, arreglos al Catalán

---
+++ mod/mgm/lang/eu_utf8/mgm.php
@@ -0,0 +1,201 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Basque strings for mgm
+ *
+ * @package   mod_mgm
+ * @copyright 2011 José Miguel Andonegi <jm.andonegi@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activar'] = 'Gaitu';
+$string['activaedicion'] = 'Edizio hau aktibatzera zoaz; ziur al zaude?<br /><br /><small>(Eragiketa honek ezgaituko du gaituta dagoen edozein edizio)</small>';
+$string['activededicion'] = '$a ondo gaitu da';
+$string['activing'] = '$a gaitzen';
+$string['addedicion'] = 'Edizio berria gehitu';
+$string['addsel'] = 'Ikastaroa(k) gehitu';
+$string['addespec'] = 'Espezialitatea(k) gehitzen';
+$string['addtutortogroup'] = 'Tutorea taldeetan gehitu';
+$string['allcourses'] = 'Ikastaro guztiak ($a)';
+$string['alreadyadded'] = 'Dagoeneko MKM hasiera-orrian dago';
+$string['alumnos'] = 'Ikasleak';
+$string['asignado'] = 'Esleitu';
+$string['borrador'] = 'Zirriborro moduan Gorde';
+$string['borrador_txt'] = '(Zirriborroa)';
+$string['cannotaddtocourses'] = 'Ezin duzu MKM instantzia ikastarotan gehitu.';
+$string['cavailable'] = 'Ikastaro eskuragarriak';
+$string['cc'] = 'Ikastetxe kodea';
+$string['cc_mixin'] = 'Itundua';
+$string['cc_noidea'] = 'Ezezaguna';
+$string['cc_no_error'] = 'Zehaztutako ikastetxe kodea ez dago, mesedez zuzendu akatsa';
+$string['cc_private'] = 'Pribatua';
+$string['cc_private_error'] = 'Zehaztutako ikastetxe kodea ikastetxe pribatu bati dagokio';
+$string['cc_public'] = 'Publikoa';
+$string['cc_type'] = 'Ikastetxe mota';
+$string['cconcedido'] = '<b>Ikastaro onartua: </b>';
+$string['cconcedidono'] = '<b>Edizio honetan ez da ikastarorik onartu.</b>';
+$string['cdepend'] = 'Ikastaroak menpekotasunik al du?';
+$string['centrosfile'] = 'Ikastetxeen datuak dituen CSV fitxategia';
+$string['cert'] = 'Zirriborro moduan ziurtatu';
+$string['certdraft'] = 'Zirriborroaren ziurtatzea balioztatu';
+$string['certdraftsure'] = 'Edizioaren ziurtatzearen zirriborroa sortzear zaude; ziur al zaude?';
+$string['certdrafttitle'] = 'Ziurtatzearen zirriborroa sortu';
+$string['certificaciones'] = 'Ziurtatzeak';
+$string['certified'] = 'Ziurtatua';
+$string['certondraft'] = 'Edizioaren ziurtatze zirriborroa sortuta';
+$string['certonvalidate'] = 'Edizioaren ziurtatze zirriborroa egiaztatuta';
+$string['comunidad'] = 'Autonomia-erkidegoa';
+$string['certvalidatesure'] = 'Edizioaren ziurtatzearen zirriborroa egiaztatzear zaude; ziur al zaude?';
+$string['createcriteria'] = 'Irizpide berriak sortu';
+$string['createedition'] = 'Edizio berria sortu';
+$string['criterios'] = 'Izen-emate irizpideak ';
+$string['cselected'] = 'Hautatutako ikastaroak';
+$string['codemessage'] = 'Zure ikastetxetik kanpo bazaude (zerbitzu-eginkizuna edo antzekoa), telefonoz deitu egiguzu ordezko helbidea jakinarazteko.';
+$string['configcentrosfile'] = 'Lehenetsitako ikastetxeak definitzen dituen CSV fitxategiaren bide-izena (fitxategiaren formatua: Identifikadorea Izena).';
+$string['configemailnotification'] = 'Gaituz gero, erabiltzaileek matrikula baieztapen bat jasoko dute bere postontzi elektronikoan.';
+$string['configrevisioninformation'] = 'Matrikulazioak berrikusterakoan <b>informazioa modulutik</b> zutabean erakutsiko diren parametroak aukeratu.';
+$string['confirmar_borrador'] = 'Zirriborroa baieztatu';
+$string['controlpanel'] = 'ITE-ren kontrol-panela';
+$string['coordinador'] = 'Koordinatzailea';
+$string['deactiving'] = '$a ezgaitzen ';
+$string['deactivededicion'] = '$a ondo ezgaitu da';
+$string['deleteedicion'] = 'Edizioa ezabatu';
+$string['deletededicion'] = '$a ondo ezabatu da';
+$string['deleteedicioncheck'] = 'Edizio hau eta bere edukia ezabatuko da; ziur al zaude?';
+$string['deletingedition'] = '$a ezabatzen';
+$string['desactivar'] = 'Ezgaitu';
+$string['desactivaedicion'] = 'Edizio hau ezgaituko duzu; ziur al zaude?';
+$string['dni'] = 'Dokumentuaren identifikadorea';
+$string['dnimulti'] = 'Dagoeneko existitzen da erabiltzaile bat sistema NAN horrekin.';
+$string['dninotvalid'] = 'Sartutako NAN ez da IFZ, IFK edo AIZ onargarri bat.';
+$string['edicionactiva'] = 'Ezin da editatu gaitutako edizio bat.';
+$string['edicion'] = 'Edizioa';
+$string['edicionesaddress'] = 'Ikasleen datu hedatuak editatu';
+$string['edicioncursos'] = 'Ikastaroetan aurretiko izen-ematea lehentasunen arabera';
+$string['edicioncriteria'] = 'Izen-emate irizpideak esleitu';
+$string['ediciones'] = 'Edizioak';
+$string['edicionesaprobe'] = 'Izen-emateak onartu';
+$string['edicionescert'] = 'Ziurtatutako ikastaroak berrikusi';
+$string['edicionescoursemgmt'] = 'Izen-emate irizpideak esleitu';
+$string['edicionesmgmt'] = 'Edizioak gehitu/aldatu';
+$string['edicionesrole'] = 'Zertifikaziorako rolak finkatu';
+$string['edicionesscala'] = 'Ikastaroen zertifikazio-eskala finkatu';
+$string['edicionwarning'] = '<center><b>Kontuz: edizio honetako lehentasunak aldatzen badituzu, zure aurretiko izen-ematearen data aldatua izango da eta datu hau kontuan hartuko da behin-betiko izen-ematea egiteko garaian.</b></center>';
+$string['editeditionsettings'] = 'Edizioaren doikuntzak editatu';
+$string['editionname'] = 'Edizioaren izena';
+$string['emailnotification'] = 'Posta-elektroniko bidezko jakinarazpena gaitu.';
+$string['especdata'] = 'Espezialitateak sortu/ikusi';
+$string['especialidades'] = 'Espezialitateak';
+$string['fechaemision'] = 'Ziurtagiriaren igorpen-data';
+$string['fechafin'] = 'Amaiera data';
+$string['fechainscripcion'] = 'Izen-emate data';
+$string['fechainicio'] = 'Hasiera data';
+$string['fueradeperiodo'] = 'Ezin duzu egin aurretiko izen-ematea berrikusitako edizio batean';
+$string['groups'] = 'Ikastaroaren taldeak';
+$string['inscribe'] = 'Izena eman';
+$string['maxgroup'] = 'Talde bakoitzeko gehienezko erabiltzaile kopurua';
+$string['mgm'] = 'Matrikulazioa eta kudeaketa';
+$string['mgm:viewedicion'] = 'Edizioa ikusi';
+$string['mgm:createedicion'] = 'Edizioa sortu';
+$string['mgm:editedicion'] = 'Edizioa aldatu';
+$string['mgm:assigncriteria'] = 'Irizpideak esleitu';
+$string['mgm:aprobe'] = 'Matrikulatu';
+$string['mgmfieldset'] = 'Pertsonalizatutako adibidea';
+$string['mgmname'] = 'Moodle-entzako matrikulazioa eta kudeaketa';
+$string['mgmuser'] = 'Irakaslearen datuak editatu';
+$string['mgmrequest'] = 'Aurretiko izen-emateak ikusi';
+$string['mingroup'] = 'Taldeko erabiltzale kopuru minimoa';
+$string['modulename'] = 'Matrikulazioa eta kudeaketa';
+$string['modulenameplural'] = 'mkmak';
+$string['modquestion'] = '<b>Matrikulazio eta Kudeaketaren instantzia bat gehituko duzu; ziur al zaude?</b><br />Eragiketa hau hasiera-orrian soilik egin beharko litzateke...';
+$string['newaddress'] = 'Ordezko helbidea';
+$string['newedition'] = 'Edizio berria sortu';
+$string['noactiveedition'] = 'Edizioa ez dago gaituta';
+$string['nodata'] = 'Eragiketa burutzeko behar diren datuak ez dira eman.';
+$string['nodependencias'] = 'Ezin duzu izena eman ikastaro honetan aurretiko dependentziak ez dituzulako gainditu';
+$string['noeditioncourse'] = 'Ikastaro hau ez dago inongo edizioaren menpe.';
+$string['nofecha'] = 'Ziurtatzearen igortze-data bat zehaztu behar duzu';
+$string['nohaydatos'] = 'Sisteman ez dago edizioetara aurretiko izen-emate daturik zuri lotuta';
+$string['nocerthistory'] = 'Erabiltzailearen historian ez dago ziurtaketarik';
+$string['nocourses'] = 'Ez da topatu ikastarorik';
+$string['noplaces'] = '\'$a->alumnos\' ikasleen izen-eamtea tramitatu nahi duzu baina bakarrik \'$a->plazas\' leku libre daude.<br /><br />Izen-ematearekin jarraitu nahi duzu?';
+$string['numeric'] = 'Zenbakizko balio bat sartu behar duzu';
+$string['numgroups'] = 'Talde kopurua';
+$string['numregistro'] = 'Erabiltzaile zenbakia';
+$string['opcion'] = 'Aukera';
+$string['opcionesduplicadas'] = 'Lehentasun-aukera bat bi aldiz hautatu duzu, konpondu arazoa';
+$string['opcionuno'] = 'Lehen aukera';
+$string['opciondos'] = 'Bigarren aukera';
+$string['out'] = 'Lekurik gabe';
+$string['pageprint'] = 'Inprimatu';
+$string['pago'] = 'Edizioaren ordainketa-txostena jaitsi';
+$string['pago-nc'] = 'Edizioa ez dago ziurtatuta';
+$string['plazas'] = 'Lekuak';
+$string['posalumno'] = 'Postua';
+$string['preinscrito'] = 'Ikastaro batean aurretiko izen-ematea burutu duzu. Nahi duzu beste ikastaro batean aurretiko izen-ematea egin?';
+$string['prioridadcentro'] = 'HM lurraldeko ikastetxeen lehentasunak';
+$string['prioridadespec'] = 'Espezialitateen lehentasunak';
+$string['removesel'] = 'Ikastaroa(k) ezabatu';
+$string['removeespec'] = 'Espezialitatea(k) ezabatu';
+$string['reviewnotaprobed'] = 'Izen-ematerik gabeko ikasleak ikusi';
+$string['revisioninformation'] = 'Berrikuste-parametroak erakutsi';
+$string['rolesdone'] = 'Ziurtatze-rolak finkatuta';
+$string['rollback_borrador'] = 'Zirriborroa ezeztatu';
+$string['scaladone'] = 'Ziurtatze-eskala gordeta';
+$string['searchmgm'] = 'Edizioak bilatu';
+$string['selectedlist'] = 'Hautatutako ikastaro-zerrenda...';
+$string['selectedespeclist'] = 'Hautatutako espezialitate-zerrenda...';
+$string['setroles'] = 'Ziurtatzerako rolak finkatu';
+$string['setscala'] = 'Ziurtatze-eskala finkatu';
+$string['showcertifications'] = 'Ziurtatzeak ikusi';
+$string['sinespecialidades'] = 'Espezialitateak ez daude konfiguratuta';
+$string['sinprioridad'] = 'Lehentasunik ez';
+$string['solicitudes'] = 'Eskaerak';
+$string['sysnewaddress'] = 'Ikastetxearen helbidearen ordez, ordezko helbidea erabiliko da.';
+$string['tutor'] = 'Tutorea';
+$string['userselect'] = 'Erabiltzailea hautatu';
+$string['exportdata'] = 'Hirugarrenentzako datuak esportatu';
+$string['tipoid'] = 'Identifikazio dokumentu mota';
+$string['codniveleducativo'] = 'Ikasketa maila kodea';
+$string['codcuerpodocente'] = 'Irakasleen kidego kodea';
+$string['codpostal'] = 'Posta kodea';
+$string['sexo'] = 'Sexua';
+$string['course_extend'] = 'Ikastaroaren datu hedatuak';
+$string['codagrupacion'] = 'Taldekatze kodea';
+$string['codmodalidad'] = 'Modalitate kodea';
+$string['codprovincia'] = 'Probintzia kodea';
+$string['codpais'] = 'Herrialde kodea';
+$string['codmateria'] = 'Ikasgai kodea';
+$string['codniveleducativo'] = 'Ikasketa maila kodea';
+$string['numhoras'] = 'Ordu kopurua';
+$string['numcreditos'] = 'Kreditu kopurua';
+$string['fechainicio'] = 'Hasiera data';
+$string['fechafin'] = 'Amaiera data';
+$string['localidad'] = 'Herrialdea';
+$string['fechainimodalidad'] = 'Modalitatearen hasiera data';
+$string['edition_not_ended'] = '<p>Oraingo edizioa ez da amaitu</p>';
+$string['user_no_task_ended'] = '<p>\"$a->curso\" ikastaroko $a->tipo $a->usuario erabiltzaileak ez du <a href=\"../feedback/view.php?id=$a->tareaid\"/>\"$a->tarea\"</a> zeregina amaitu.</p>';
+$string['file_export_link'] = '<p>Sakatu hemen <a href=\"export.php?filename=$a->filename\">fitxategia</a>  jaisteko.</p>';
+$string['incidencia_curso'] = '<p>Kontuz: ez dago daturik \"$a->curso\" ikastaroko $a->campo eremuan. <a href=\"courses.php?courseid=$a->cursoid&edicionid=$a->edicionid\"/>Hemen</a> editatu dezakezu.</p>';
+$string['incidencia_usuario'] = '<p>Kontuz: ez dago datu hedaturik $a->nombre erabiltzailearentzat. Sakatu <a href=\"../../course/loginas.php?user=$a->id&sesskey=$a->sesskey\"/>hemen</a> erabiltzaile horrekin saioa irekitzeko eta ITE-ren kontrol-panelean irakasle informazioa editatzeko.</p>';
+$string['incidencia_dni'] = '<p>Kontuz: NAN eremua hutsik dago edo akatsduna da $a->nombre erabiltzailearentzat. sakatu <a href=\"../../course/loginas.php?user=$a->id&sesskey=$a->sesskey\"/>hemen</a> erabiltzaile horrekin saioa irekitzeko eta ITE-ren kontrol-panelean irakasle informazioa editatzeko.</p>';
+$string['incidencia_no_aprobado'] = '<p>Kontuz: $a->nombre erabiltzailea ez da sartuko ez duelako \"$a->curso\" ikastaroa gainditu.</p>';
+$string['incidencia_no_tipo_usuario'] = '<p>Kontuz: $a->nombre erabiltzaileari ez zaio Tutore, Ikasle edo Koordinatzaile rola esleitu \"$a->curso\" ikastaroan.</p>';
